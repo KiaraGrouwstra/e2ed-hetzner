b7add5777f25ff70b6c0683718719c1ac48734b1
kiara <kiara@bij1.org>
2022-08-13 20:32:23 +0000

    backend: http -> cloud

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
 terraform {
-  backend "http" {}
+
+  cloud {
+    hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
+    organization = "bij1"
+    workspaces {
+      name = "infra"
+    }
+  }
+
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
+
 }
 
 provider "digitalocean" {}
