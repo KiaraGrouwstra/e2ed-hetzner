51a0dd47d8731c903c71f6162ded0e1827f5198a
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-06-08 10:23:20 +0000

    format

--- config.nix
+++ config.nix
@@ -1,9 +1,15 @@
-{ config, lib, inputs, pkgs, options, specialArgs, ... }:
-
-let
+{
+  config,
+  lib,
+  inputs,
+  pkgs,
+  options,
+  specialArgs,
+  ...
+}: let
   var = options.variable;
 
-  my-lib = import ./lib/default.nix { inherit lib; };
+  my-lib = import ./lib/default.nix {inherit lib;};
 
   # (k: k + k) -> { a = 1; } -> { aa = 1; }
   mapKeys = f: lib.mapAttrs' (k: v: lib.nameValuePair (f k) v);
@@ -18,19 +24,14 @@ let
   inNamespace = prefix: mapKeys (k: "${prefix}_${k}");
 
   # { a = 1; } -> { name = "a"; a = 1; }
-  setNames = lib.mapAttrs (k: v: { name = k; } // v);
+  setNames = lib.mapAttrs (k: v: {name = k;} // v);
 
   hetzner = let
-
     # https://docs.hetzner.com/cloud/api/getting-started/generating-api-token
     token = lib.tfRef "var.hcloud_api_token";
-
-  in { inherit token; };
-
+  in {inherit token;};
 in rec {
-
   terraform = {
-    
     cloud = {
       hostname = "app.terraform.io";
       organization = "bij1";
@@ -38,28 +39,23 @@ in rec {
         name = "hcloud";
       };
     };
-
   };
 
   provider = {
-
     # Configure the Hetzner Cloud Provider
     hcloud.token = lib.mkForce hetzner.token;
-
   };
 
-  resource = (inNamespace "hcloud" {
-
-    ssh_key = setNames
-      (lib.mapAttrs (_: v: { public_key = v; })
-      (my-lib.dirContents ".pub" ./ssh-keys));
-
-  });
+  resource = inNamespace "hcloud" {
+    ssh_key =
+      setNames
+      (lib.mapAttrs (_: v: {public_key = v;})
+        (my-lib.dirContents ".pub" ./ssh-keys));
+  };
 
   # Set the variable value in *.tfvars file
   # or using -var="hcloud_api_token=..." CLI option
   variable = {
-
     # suppress warning, otherwise unused here
     tf_cloud_token = {
       type = "string";
@@ -71,30 +67,23 @@ in rec {
       description = "[Hetzner Cloud API Token](https://docs.hetzner.com/cloud/api/getting-started/generating-api-token)";
       sensitive = true;
     };
-
   };
 
   # https://github.com/terranix/terranix-hcloud/blob/main/options.md
   hcloud = {
     enable = true;
     # can also be specified with the TF_VAR_hcloud_api_token environment variable
-    provider = { inherit (hetzner) token; };
+    provider = {inherit (hetzner) token;};
     export.nix = "hetzner.nix";
-
   };
 
   data = {
-
     hcloud_ssh_keys."all_keys" = {};
-
   };
 
   output = {
-
     "keys_output" = {
       value = lib.tfRef "data.hcloud_ssh_keys.all_keys";
     };
-
   };
-
 }
