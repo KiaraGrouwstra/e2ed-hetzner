49600290fad180aa5466d1b61deedf754e3cbff9
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-09-29 23:02:32 +0200

    update interface name: enp1s0 -> enp7s0

--- servers/common/net.nix
+++ servers/common/net.nix
@@ -10,7 +10,7 @@
       "2a01:4ff:ff00::add:2"
       "2a01:4ff:ff00::add:1"
     ];
-    # interfaces.enp1s0.ipv6.addresses = [
+    # interfaces.enp7s0.ipv6.addresses = [
     #   {
     #     # address = pconf.vps.ip6;
     #     prefixLength = 64;
@@ -18,7 +18,7 @@
     # ];
     defaultGateway6 = {
       address = "fe80::1";
-      interface = "enp1s0";
+      interface = "enp7s0";
     };
     firewall.enable = true;
     firewall.allowedTCPPorts = [ 80 443 ];
@@ -26,7 +26,7 @@
     nat = {
       enable = true;
       internalInterfaces = ["ve-+"];
-      externalInterface = "enp1s0";
+      externalInterface = "enp7s0";
       # Lazy IPv6 connectivity for the container
       enableIPv6 = true;
     };
