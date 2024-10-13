3fb2feb8da35f948e1bc1f08d41de3b95847751a
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-10-13 13:55:05 +0200

    rm tf-info.sh as i did not figure out how to pass in generated files not in version control

--- tf-info.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-tofu show -json | jq 'def by(k): group_by(.[k]) | map({(.[0].[k]): .}) | add; .values.root_module.resources | by("mode") | map_values(by("type") | map_values(map({(.name): .}) | add))' > terraform.json
