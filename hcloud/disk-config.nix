edfb51628bafef6339c968acab62821f062c2fd2
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-10-15 18:14:08 +0200

    use zfs

--- hcloud/disk-config.nix
+++ hcloud/disk-config.nix
@@ -1,54 +1,89 @@
 {lib, ...}: {
+  # https://github.com/nix-community/nixos-anywhere/issues/179
+  boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "thunderbolt" "usb_storage" "usbhid" "sd_mod"];
+  boot.supportedFilesystems = ["btrfs" "ext2" "ext3" "ext4" "exfat" "f2fs" "fat8" "fat16" "fat32" "ntfs" "xfs"];
+
+  # needed for ZFS, get using say `head -c 8 /etc/machine-id`
+  networking.hostId = "deadbeef";
+  # networking.hostId = lib.substring 0 8 (lib.readFile "/etc/machine-id");
+  # error: access to absolute path '/etc/machine-id' is forbidden in pure eval mode
+  # TODO: reuse server.values.id?
+
   disko.devices = {
-    disk.disk1 = {
-      device = lib.mkDefault "/dev/sda";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+    disk = {
+      disk1 = {
+        device = lib.mkDefault "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
             };
-          };
-          root = {
-            name = "root";
-            size = "100%";
-            content = {
-              type = "lvm_pv";
-              vg = "pool";
+            esp = {
+              name = "ESP";
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
             };
-          };
-        };
-      };
-    };
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
-              mountOptions = [
-                "defaults"
-              ];
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
             };
           };
         };
       };
     };
+
+    # https://github.com/nix-community/disko-templates/blob/main/zfs-impermanence/disko-config.nix
+    zpool = {
+      zroot = {
+        type = "zpool";
+        rootFsOptions = {
+          acltype = "posixacl";
+          atime = "off";
+          compression = "zstd";
+          mountpoint = "none";
+          xattr = "sa";
+        };
+        options.ashift = "12";
+        datasets = {
+          "local" = {
+            type = "zfs_fs";
+            options.mountpoint = "none";
+          };
+          "local/root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+          };
+          "local/nix" = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+          };
+          "local/persist" = {
+            type = "zfs_fs";
+            mountpoint = "/persist";
+          };
+          "local/log" = {
+            type = "zfs_fs";
+            mountpoint = "/var/log";
+          };
+        };
+      };
+    };
+  };
+
+  fileSystems = {
+    "/persist".neededForBoot = true;
+    "/var/log".neededForBoot = true;
   };
 }
